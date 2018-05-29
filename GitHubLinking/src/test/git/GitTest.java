f31e64c8025fd24f73eddc172d486476ccef212e
kosmo05 <kosmo05@DESKTOP-26RO710>
2018-05-29 11:00:53 +0900

    test

--- GitHubLinking/src/test/git/GitTest.java
+++ GitHubLinking/src/test/git/GitTest.java
@@ -3,7 +3,7 @@ package test.git;
 public class GitTest {
 	public static void main(String args[]) {
 		
-		char upperChar = 'A';
+		char upperChar = 's';
 		char lowerChar ='a';
 		
 		for (int i = 0; i < 26; i++) {
